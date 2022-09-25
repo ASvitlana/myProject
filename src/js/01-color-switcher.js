5e9eab5bb557ff0f84e7d955c9e1a0b36f709f41
Светлана Петровна Андрела <104157616+ASvitlana@users.noreply.github.com>
2022-09-25 20:31:42 +0300

    add Task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+let timerId = null;
+stopButton.disabled = true;
+
+startButton.addEventListener('click', startChangingColor);
+stopButton.addEventListener('click', stopChangingColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function startChangingColor() {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    
+    timerId = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+    }, 1000);
+};
+
+function stopChangingColor() {
+    clearInterval(timerId);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+};
+
+
 
